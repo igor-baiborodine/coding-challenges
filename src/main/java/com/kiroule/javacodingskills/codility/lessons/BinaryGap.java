3642f2b96a30f9e94d94c407f221d75c13ddf308
Igor Baiborodine <ibaiborodine@gmail.com>
2017-10-22 21:42:17 -0400

    Initial commit

---
+++ src/main/java/com/kiroule/javacodingskills/codility/lessons/BinaryGap.java
@@ -0,0 +1,12 @@
+package com.kiroule.javacodingskills.codility.lessons;
+
+/**
+ * @author Igor Baiborodine
+ */
+public class BinaryGap {
+
+  public int solution(int n) {
+    // TODO: implement me
+    return 0;
+  }
+}
