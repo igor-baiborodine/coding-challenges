e8fd96578b5835004ef64f5ab4af6897205407e3
Igor Baiborodine <ibaiborodine@gmail.com>
2017-10-31 20:29:30 -0400

    Initial commit for the Tape Equilibrium exercise.

---
+++ src/main/java/com/kiroule/javacodingskills/codility/lesson3/TapeEquilibrium.java
@@ -0,0 +1,15 @@
+package com.kiroule.javacodingskills.codility.lesson3;
+
+/**
+ * @author Igor Baiborodine
+ *
+ * @see ../TAPE_EQUILIBRIUM_README.md
+ */
+public class TapeEquilibrium {
+
+  public int solution(int[] A) {
+    // TODO: implement me
+    return 0;
+  }
+
+}
---
+++ src/test/java/com/kiroule/javacodingskills/codility/lesson3/TapeEquilibriumTest.java
@@ -0,0 +1,19 @@
+package com.kiroule.javacodingskills.codility.lesson3;
+
+import org.junit.Test;
+
+/**
+ * @author Igor Baiborodine
+ */
+public class TapeEquilibriumTest {
+
+  @Test
+  public void solution() throws Exception {
+    // given
+
+    // when
+
+    // then
+  }
+
+}
