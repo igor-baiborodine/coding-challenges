71a71621eef6044f90a809083bcb372d89e13064
Igor Baiborodine <ibaiborodine@gmail.com>
2017-10-31 20:19:05 -0400

    Implement solution for the Cyclic Solution exercise.

--- src/main/java/com/kiroule/javacodingskills/codility/lesson2/CyclicRotation.java
+++ src/main/java/com/kiroule/javacodingskills/codility/lesson2/CyclicRotation.java
@@ -8,13 +8,24 @@ package com.kiroule.javacodingskills.codility.lesson2;
 public class CyclicRotation {
 
   public int[] solution(int[] A, int K) {
-
-    if (A.length <=1
-        || A.length == K) {
+    if (A.length <= 1
+        || (K >= A.length && K % A.length == 0)) {
       return A;
     }
-    // TODO: complete implementation
-    return new int[] {};
+    if (K > A.length) {
+      K = K % A.length;
+    }
+
+    rotations:
+    for (int i = 0; i < K; i++) {
+      int lastElement = A[A.length - 1];
+      elements:
+      for (int j = A.length - 1; j > 0; j--) {
+        A[j] = A[j - 1];
+      }
+      A[0] = lastElement;
+    }
+    return A;
   }
 
 }
--- src/test/java/com/kiroule/javacodingskills/codility/lesson2/CyclicRotationTest.java
+++ src/test/java/com/kiroule/javacodingskills/codility/lesson2/CyclicRotationTest.java
@@ -3,8 +3,8 @@ package com.kiroule.javacodingskills.codility.lesson2;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertThat;
 
 /**
  * @author Igor Baiborodine
@@ -32,7 +32,6 @@ public class CyclicRotationTest {
     // when
     int[] result = cyclicRotation.solution(input, rotations);
     // then
-    assertThat(result.length, is(input.length));
     assertArrayEquals(input, result);
   }
 
@@ -44,18 +43,73 @@ public class CyclicRotationTest {
     // when
     int[] result = cyclicRotation.solution(input, rotations);
     // then
-    assertThat(result.length, is(input.length));
     assertArrayEquals(input, result);
   }
 
-  // TODO: implement more unit tests
   @Test
-  public void solution() throws Exception {
+  public void solution_fiveElementArrayAndTenRotations_arrayRotatedFiveTimes() throws Exception {
     // given
-
+    int[] input = new int[] {1, 2, 3, 4, 5};
+    int rotations = 10;
     // when
-
+    int[] result = cyclicRotation.solution(input, rotations);
     // then
+    assertArrayEquals(input, result);
+  }
+
+  @Test
+  public void solution_fourElementArrayAndOneRotation_arrayRotatedOneTime() throws Exception {
+    // given
+    int[] input = new int[] {1, 2, 3, 4};
+    int rotations = 1;
+    // when
+    int[] result = cyclicRotation.solution(input, rotations);
+    // then
+    assertArrayEquals(new int[] {4, 1, 2, 3}, result);
+  }
+
+  @Test
+  public void solution_fourElementArrayAndTwoRotations_arrayRotatedTwoTimes() throws Exception {
+    // given
+    int[] input = new int[] {1, 2, 3, 4};
+    int rotations = 2;
+    // when
+    int[] result = cyclicRotation.solution(input, rotations);
+    // then
+    assertArrayEquals(new int[] {3, 4, 1, 2}, result);
+  }
+
+  @Test
+  public void solution_fourElementArrayAndThreeRotations_arrayRotatedThreeTimes() throws Exception {
+    // given
+    int[] input = new int[] {1, 2, 3, 4};
+    int rotations = 3;
+    // when
+    int[] result = cyclicRotation.solution(input, rotations);
+    // then
+    assertArrayEquals(new int[] {2, 3, 4, 1}, result);
+  }
+
+  @Test
+  public void solution_fourElementArrayAndSixRotations_arrayRotatedTwoTimes() throws Exception {
+    // given
+    int[] input = new int[] {1, 2, 3, 4};
+    int rotations = 6;
+    // when
+    int[] result = cyclicRotation.solution(input, rotations);
+    // then
+    assertArrayEquals(new int[] {3, 4, 1, 2}, result);
+  }
+
+  @Test
+  public void solution_fiveElementArrayAndThreeRotations_arrayRotatedThreeTimes() throws Exception {
+    // given
+    int[] input = new int[] {3, 8, 9, 7, 6};
+    int rotations = 3;
+    // when
+    int[] result = cyclicRotation.solution(input, rotations);
+    // then
+    assertArrayEquals(new int[] {9, 7, 6, 3, 8}, result);
   }
 
 }
