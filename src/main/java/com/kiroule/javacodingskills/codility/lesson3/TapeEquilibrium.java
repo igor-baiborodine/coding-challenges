4dea8ae9fb8509adf269d072de8df9ec581a7825
Igor Baiborodine <ibaiborodine@gmail.com>
2017-11-09 22:47:43 -0500

    Implement solution for the Tape Equilibrium exercise.

--- src/main/java/com/kiroule/javacodingskills/codility/lesson3/TapeEquilibrium.java
+++ src/main/java/com/kiroule/javacodingskills/codility/lesson3/TapeEquilibrium.java
@@ -8,8 +8,21 @@ package com.kiroule.javacodingskills.codility.lesson3;
 public class TapeEquilibrium {
 
   public int solution(int[] A) {
-    // TODO: implement me
-    return 0;
+    for (int i = 1; i < A.length; i++) {
+      A[i] = A[i - 1] + A[i];
+    }
+    int minDiff = Math.abs(A[0] * 2 - A[A.length - 1]);
+    //int minDiffIndex = 1;
+
+    for (int i = 1; i < A.length - 1; i++) {
+      int diff = Math.abs(A[i] * 2 - A[A.length - 1]);
+      if (diff < minDiff) {
+        minDiff = diff;
+        //minDiffIndex = i + 1;
+      }
+    }
+    //System.out.println("min diff index: " + minDiffIndex);
+    return minDiff;
   }
 
 }
--- src/test/java/com/kiroule/javacodingskills/codility/lesson3/TapeEquilibriumTest.java
+++ src/test/java/com/kiroule/javacodingskills/codility/lesson3/TapeEquilibriumTest.java
@@ -1,19 +1,32 @@
 package com.kiroule.javacodingskills.codility.lesson3;
 
 import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
 /**
  * @author Igor Baiborodine
  */
 public class TapeEquilibriumTest {
 
+  private TapeEquilibrium tapeEquilibrium = new TapeEquilibrium();
+
   @Test
   public void solution() throws Exception {
-    // given
+    assertThat(tapeEquilibrium.solution(new int[]{0, 0}), is(0));
+    assertThat(tapeEquilibrium.solution(new int[]{1, 0}), is(1));
+    assertThat(tapeEquilibrium.solution(new int[]{-1, 0}), is(1));
+    assertThat(tapeEquilibrium.solution(new int[]{1, -1}), is(2));
+    assertThat(tapeEquilibrium.solution(new int[]{1, 1}), is(0));
 
-    // when
+    assertThat(tapeEquilibrium.solution(new int[]{1, 1, 1}), is(1));
+    assertThat(tapeEquilibrium.solution(new int[]{1, -1, 1}), is(1));
+    assertThat(tapeEquilibrium.solution(new int[]{1, 2, 3}), is(0));
 
-    // then
+    assertThat(tapeEquilibrium.solution(new int[]{3, 1, 2, 4, 3}), is(1));
+    assertThat(tapeEquilibrium.solution(new int[]{1, 1, 10, 10, 1, 10}), is(9));
+    assertThat(tapeEquilibrium.solution(new int[]{1, 1, 10, 10, 1, -10}), is(9));
+    assertThat(tapeEquilibrium.solution(new int[]{1, 1, -10, 10, 1, -10}), is(9));
   }
 
 }
