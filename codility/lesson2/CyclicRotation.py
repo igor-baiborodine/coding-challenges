d0057167b0cf76934a4e8a69e89858dbdb75f278
Igor Baiborodine <igor.baiborodine@yandex.com>
2018-03-15 15:25:17 -0400

    Update CyclicRotation.py

--- codility/lesson2/CyclicRotation.py
+++ codility/lesson2/CyclicRotation.py
@@ -1,3 +1,8 @@
+""" See ../CYCLIC_ROTATION_README.md
+
+Author: Igor Baiborodine
+"""
+
 def solution(A, K):
     if len(A) <= 1 or (K >= len(A) and K % len(A) == 0):
         return A
@@ -12,3 +17,12 @@ def solution(A, K):
         K -= 1
 
     return A
+
+assert solution([], 0) == []
+assert solution([1], 0) == [1]
+assert solution([1, 2, 3, 4, 5], 5) == [1, 2, 3, 4, 5]
+assert solution([1, 2, 3, 4, 5], 10) == [1, 2, 3, 4, 5]
+assert solution([1, 2, 3, 4], 2) == [3, 4, 1, 2]
+assert solution([1, 2, 3, 4], 3) == [2, 3, 4, 1]
+assert solution([1, 2, 3, 4], 6) == [3, 4, 1, 2]
+assert solution([3, 8, 9, 7, 6], 3) == [9, 7, 6, 3, 8]
