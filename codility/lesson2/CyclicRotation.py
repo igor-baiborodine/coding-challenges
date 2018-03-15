edeb5fef713b5598ba249755410006bb280ea9f3
Igor Baiborodine <igor.baiborodine@yandex.com>
2018-03-15 15:09:03 -0400

    Update CyclicRotation.py

--- codility/lesson2/CyclicRotation.py
+++ codility/lesson2/CyclicRotation.py
@@ -1,1 +1,14 @@
+def solution(A, K):
+    if len(A) <= 1 or (K >= len(A) and K % len(A) == 0):
+        return A
+  
+    if K > len(A):
+        K = K % len(A)
+  
+    while (K > 0):
+        last_elem = A[len(A) - 1]
+        A.insert(0, last_elem)
+        del A[len(A) - 1]
+        K -= 1
 
+    return A
