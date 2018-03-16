f6a3a944131c7d00a2b6331e277ef95b7fbe5f7d
igor-baiborodine <igor.baiborodin@intact.net>
2018-03-16 15:11:14 -0400

    Implement solution for the Frog Jump exercise

--- codility/lesson3/FrogJump.py
+++ codility/lesson3/FrogJump.py
@@ -5,6 +5,23 @@ Author:   Igor Baiborodine
 
 """
 
+import math
 
-def solution(x, y, d)
-    pass
+
+def solution(x, y, d):
+    jump = y - x
+    if (jump == 0):
+        return 0
+    elif jump < d:
+        return 1
+    else:
+        return math.ceil(jump/d)
+		
+		
+assert solution(1, 1, 1) == 0
+assert solution(1, 2, 1) == 1
+assert solution(1, 2, 2) == 1
+assert solution(1, 3, 1) == 2
+assert solution(1, 6, 2) == 3
+assert solution(10, 85, 30) == 3	
+		
