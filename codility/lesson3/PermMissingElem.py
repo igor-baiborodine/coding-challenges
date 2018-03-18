0d1148f92acb6232759580a0953521bd05c27fb7
Igor Baiborodine <igor.baiborodine@yandex.com>
2018-03-18 08:40:23 -0400

    Implement solution for Permutation Missing Element exercise

---
+++ codility/lesson3/PermMissingElem.py
@@ -0,0 +1,24 @@
+"""Solution for the Permutation Missing Element exercise.
+
+Exercise: ../PERM_MISSING_ELEM_README.md
+Author:   Igor Baiborodine
+
+"""
+
+
+def solution(a):
+    perm_sum = 0
+    for each in a:
+        perm_sum += each
+
+    full_perm_sum = ((len(a) + 1) * (len(a) + 2)) // 2
+    return full_perm_sum - perm_sum
+
+
+assert solution([]) == 1
+assert solution([1]) == 2
+assert solution([2]) == 1
+assert solution([1, 2]) == 3
+assert solution([3, 1]) == 2
+assert solution([2, 3]) == 1
+assert solution([2, 3, 1, 5]) == 4
