6f1dfd6b5423cd8d64e5395c0ceb7a770fbba768
Igor Baiborodine <igor.baiborodine@yandex.com>
2018-03-18 13:39:26 -0400

    Implement solution for FrogRiverOne task

---
+++ codility/lesson4/FrogRiverOne.py
@@ -0,0 +1,37 @@
+"""Solution for the FrogRiverOne task.
+
+Task:   ../FROG_RIVER_ONE_README.md
+Author: Igor Baiborodine
+
+"""
+
+
+def solution(x, a):
+    min_sec = -1
+    counts = {}
+    for sec, pos in enumerate(a):
+        # if pos in counts:
+        #     counts[pos] += 1
+        # else:
+        #     counts[pos] = 1
+        count = counts.get(pos, 0)
+        counts[pos] = count + 1
+
+        if len(counts) == x:
+            min_sec = sec
+            break
+
+    return min_sec
+
+
+assert solution(1, [1]) == 0
+assert solution(1, [1, 1]) == 0
+assert solution(2, [1, 2]) == 1
+assert solution(2, [2, 1]) == 1
+assert solution(2, [2, 2, 1]) == 2
+assert solution(2, [1, 2, 1]) == 1
+assert solution(3, [1, 2, 2, 1, 3, 1]) == 4
+assert solution(5, [1, 3, 1, 4, 2, 3, 5, 4]) == 6
+assert solution(5, [1, 3, 1, 4, 2, 3, 3, 4]) == -1
+
+
