d5564fd0879798927dd4a65df7f6f2bf127f3c64
Igor Baiborodine <igor.baiborodine@yandex.com>
2018-03-26 20:46:49 -0400

    Implement solution for GenomicRangeQuery task

--- codility/lesson5/GenomicRangeQuery.py
+++ codility/lesson5/GenomicRangeQuery.py
@@ -6,5 +6,27 @@ Author: Igor Baiborodine
 """
 
 
-def solution(S, P, Q):
-    pass
+def solution(s, p, q):
+    impact_factors = {
+        'A': 1,
+        'C': 2,
+        'G': 3,
+        'T': 4
+    }
+
+    factors = [0] * len(p)
+    for i in range(0, len(p)):
+        query = s[p[i]:(q[i] + 1)]
+        min_impact_factor = min(
+            [impact_factors[nucl_type] for nucl_type in query])
+        factors[i] = min_impact_factor
+
+    return factors
+
+
+assert solution(['A'], [0], [0]) == [1]
+assert solution(['A', 'C'], [0], [0]) == [1]
+assert solution(['A', 'C'], [1], [1]) == [2]
+assert solution(
+    ['C', 'A', 'G', 'C', 'C', 'T', 'A'], [2, 5, 0], [4, 5, 6]) == [2, 4, 1]
+assert solution(['A'] * 1000000, [0] * 50000, [1] * 50000) == [1] * 50000
