126c16768f40d533aff3a4bc871a52d9a2c3a3f5
Igor Baiborodine <igor.baiborodine@yandex.com>
2018-04-27 16:47:07 -0400

    Update time-delta-solution.py

--- hackerrank/python/time-delta-solution.py
+++ hackerrank/python/time-delta-solution.py
@@ -9,19 +9,31 @@ def time_delta(t1, t2):
                             datetime.strptime(' '.join(t2_tokens[1:-1]), '%d %b %Y %H:%M:%S'))
     t1_tmz_token = t1_tokens[-1:][0]
     t1_tmz = (t1_tmz_token[:1], t1_tmz_token[1:3].lstrip('0'), t1_tmz_token[3:].lstrip('0'))
-    # print(datetime1, datetime2, t1_tmz)
+    # print(datetime1, t1_tmz) 
     
-    if t1_tmz[1]:
-        val = -int(t1_tmz[0] + t1_tmz[1])
-        # print("hours:", val)
-        datetime1 = datetime1 + timedelta(hours=val)
-        
-    if t1_tmz[2]:
-        val = -int(t1_tmz[0] + t1_tmz[2])
-        # print("minutes:", val)
-        datetime1 = datetime1 + timedelta(minutes=val)
+    t2_tmz_token = t2_tokens[-1:][0]
+    t2_tmz = (t2_tmz_token[:1], t2_tmz_token[1:3].lstrip('0'), t2_tmz_token[3:].lstrip('0'))
+    # print(datetime2, t2_tmz) 
+    
+    datetime1 = apply_timezone(datetime1, t1_tmz)
+    datetime2 = apply_timezone(datetime2, t2_tmz)
     # print(datetime1, datetime2)
+    
     return int(abs(((datetime1 - datetime2).total_seconds())))
+
+
+def apply_timezone(dt, tmz):
+    if tmz[1]:
+        hours_arg = -int(tmz[0] + tmz[1])
+        # print("hours arg:", hours_arg)
+        dt = dt + timedelta(hours=hours_arg)
+        
+    if tmz[2]:
+        minutes_arg = -int(tmz[0] + tmz[2])
+        # print("minutes arg:", minutes_arg)
+        dt = dt + timedelta(minutes=minutes_arg)
+        
+    return dt
     
 if __name__ == "__main__":
     t = int(input().strip())
