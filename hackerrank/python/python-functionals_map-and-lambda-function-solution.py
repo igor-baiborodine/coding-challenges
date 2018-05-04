3d8d830adf586ea0f6eee304edea16ecfbda9dd5
Igor Baiborodine <igor.baiborodine@yandex.com>
2018-05-03 21:08:40 -0400

    Implement solution for Map and Lambda Funcion task

---
+++ hackerrank/python/python-functionals_map-and-lambda-function-solution.py
@@ -0,0 +1,22 @@
+cube = lambda x: x**3
+
+fib_dict = {}
+
+def fibonacci(n):
+    if n > 0:
+        calc_fibonacci(n)
+    return list(fib_dict.values())
+    
+def calc_fibonacci(n):
+    
+    if n == 1:
+        return fib_dict.setdefault(n, n - 1)
+    elif n == 2:
+        fib_dict.setdefault(n - 1, n - 2)    
+        return fib_dict.setdefault(n, n - 1)
+    else:
+        return fib_dict.setdefault(n, calc_fibonacci(n - 2) + calc_fibonacci(n - 1));
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
