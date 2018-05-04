0af95033cc007f4f551dbf48387441265d00bb25
Igor Baiborodine <igor.baiborodine@yandex.com>
2018-05-03 21:12:20 -0400

    Update python-functionals_map-and-lambda-function-solution.py

--- hackerrank/python/python-functionals_map-and-lambda-function-solution.py
+++ hackerrank/python/python-functionals_map-and-lambda-function-solution.py
@@ -8,14 +8,13 @@ def fibonacci(n):
     return list(fib_dict.values())
     
 def calc_fibonacci(n):
-    
     if n == 1:
         return fib_dict.setdefault(n, n - 1)
     elif n == 2:
         fib_dict.setdefault(n - 1, n - 2)    
         return fib_dict.setdefault(n, n - 1)
     else:
-        return fib_dict.setdefault(n, calc_fibonacci(n - 2) + calc_fibonacci(n - 1));
+        return fib_dict.setdefault(n, calc_fibonacci(n - 2) + calc_fibonacci(n - 1))
 
 if __name__ == '__main__':
     n = int(input())
