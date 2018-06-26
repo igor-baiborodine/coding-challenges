707c5947b5ff91572565c121e0ce327729857af3
igor-baiborodine <igor.baiborodin@intact.net>
2018-06-26 09:23:24 -0400

    Add problem statement for Java Subarray task

--- hackerrank/java/DataStructuresJavaSubarray.java
+++ hackerrank/java/DataStructuresJavaSubarray.java
@@ -1,59 +1,28 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class DataStructuresJavaSubarray {
 
-	private static int count = 0;
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[] arr = new int[n];
+		int[] a = new int[n];
 
 		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
+			a[i] = sc.nextInt();
 		}
-//		System.out.println(n + ", " + Arrays.toString(arr));
+		int count = 0;
 
 		for (int i = 0; i < n; i++) {
-			computePartialSums(arr, i);
-		}
-		System.out.println(count);
-	}
+			int sum = 0;
 
-	private static void computePartialSums(int[] arr, int start) {
-		int[] partialSums;
-		if ((arr.length - start) % 2 == 1) {
-			partialSums = new int[(arr.length + 1) / 2];
-		} else {
-			partialSums = new int[arr.length / 2];
-		}
-
-		if (arr.length == 2) {
-			countNegative(arr[0]);
-			countNegative(arr[1]);
-			countNegative(arr[0] + arr[1]);
-		} else{
-			int j = 0;
-			for (int i = start; i < arr.length; i++) {
-				if (i % 2 == 1) {
-					countNegative(arr[i - 1]);
-					countNegative(arr[i]);
-					partialSums[j] = arr[i - 1] + arr[i];
-					j++;
-				} else if (i == arr.length - 1) {
-					countNegative(arr[i]);
-					partialSums[j] = arr[i];
+			for (int j = i; j < n; j++) {
+				sum += a[j];
+				if (sum < 0) {
+					count++;
 				}
 			}
-			computePartialSums(partialSums, 0);
 		}
-	}
-
-	private static void countNegative(int number) {
-		if (number < 0) {
-			count++;
-		}
+		System.out.println(count);
 	}
 
 }
